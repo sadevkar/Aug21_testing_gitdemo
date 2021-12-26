49813e975117d729c61c2ef992713102e67f0129
admin <admin@DESKTOP-A89P4RU>
2021-12-26 21:12:38 +0530

    commiting 2nd time

---
+++ src/gitDemo/My_Program.java
@@ -0,0 +1,11 @@
+package gitDemo;
+
+public class My_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Class second from second leacture");
+          System.out.println("welcome to program world");
+	}
+
+}
