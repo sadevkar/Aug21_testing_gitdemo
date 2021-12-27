656e0a674449b51ae9ed7f462bf6be3164636368
admin <admin@DESKTOP-A89P4RU>
2021-12-27 13:01:20 +0530

    approved by manager added to master

---
+++ src/gitDemo/first.java
@@ -0,0 +1,12 @@
+package gitDemo;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome");
+System.out.println("Thank u");
+System.out.println("first program for velocity class");
+	}
+
+}
